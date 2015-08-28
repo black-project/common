5d0e426f8ebaf4208ec4c364edc891991430cb1e
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2015-08-28 23:28:22 +0200

    Remove ddd-php and phpspec-code-coverage dependencies

--- src/Black/Component/Common/Infrastructure/Doctrine/Manager.php
+++ src/Black/Component/Common/Infrastructure/Doctrine/Manager.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Black package.
- *
- * (c) Alexandre Balmes <alexandre@lablackroom.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Black\Component\Common\Infrastructure\Doctrine;
 
 /**
